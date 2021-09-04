55feecf7689804433f4122824a926b6569f619d1
sgnid <sgnid@DESKTOP-UIIEPRG>
2021-09-04 15:00:12 +0300

    Add project files.

---
+++ HnidetsOleksandr.RobotChallenge/HnidetsOleksandr.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+
+namespace HnidetsOleksandr.RobotChallenge
+{
+    public class HnidetsOleksandr : IRobotAlgorithm
+    {
+        string IRobotAlgorithm.Author => throw new NotImplementedException();
+
+        RobotCommand IRobotAlgorithm.DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
