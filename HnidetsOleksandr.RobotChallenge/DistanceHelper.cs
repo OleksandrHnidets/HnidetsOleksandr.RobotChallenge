40d05b38b6d2804ce08caa7da2ee866b449c564b
Oleksandr Hnidets <sgnidets@mail.ru>
2021-09-07 13:18:50 +0300

    FirstInstanceAlgorithm

---
+++ HnidetsOleksandr.RobotChallenge/DistanceHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Robot.Common;
+
+namespace HnidetsOleksandr.RobotChallenge
+{
+    class DistanceHelper
+    {
+        public static int FindDistance(Position a, Position b)
+            => (int)(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+    }
+}
--- HnidetsOleksandr.RobotChallenge/HnidetsOleksandr.cs
+++ HnidetsOleksandr.RobotChallenge/HnidetsOleksandr.cs
@@ -16,11 +16,59 @@ namespace HnidetsOleksandr.RobotChallenge
 
         public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
         {
-            var myRobot = robots[robotToMoveIndex];
-            Position newposition = myRobot.Position;
-            newposition.X++;
-            newposition.Y++;
-            return new MoveCommand() { NewPosition = newposition };
+            Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
+            if((movingRobot.Energy>500)&&(robots.Count<map.Stations.Count))
+            {
+                return new CreateNewRobotCommand();
+            }
+
+            Position stationPosition = FindNearestStation(robots[robotToMoveIndex], map, robots);
+
+            if (stationPosition == null)
+                return null;
+            if (stationPosition == movingRobot.Position)
+                return new CollectEnergyCommand();
+            else 
+            {
+                return new MoveCommand() { NewPosition = stationPosition };
+            }
         }
+
+        public Position FindNearestStation(Robot.Common.Robot movingrobot, Map map, IList<Robot.Common.Robot> robots)
+        {
+            EnergyStation nearest = null;
+            int minDistance = int.MaxValue;
+            
+            foreach(var station in map.Stations)
+            {
+                if(IsStationFree(station,movingrobot,robots))
+                {
+                    int d = DistanceHelper.FindDistance(station.Position, movingrobot.Position);
+                    if (d < minDistance)
+                    {
+                        minDistance = d;
+                        nearest = station;
+                    }
+                }
+            }
+            return nearest == null ? null : nearest.Position;
+        }
+
+        public bool IsCellFree(Position cell, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        {
+            foreach(var robot in robots)
+            {
+                if(robot!=movingRobot)
+                {
+                    if (robot.Position == cell)
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        public bool IsStationFree(EnergyStation station, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
+        => IsCellFree(station.Position, movingRobot, robots);
     }
+
 }
