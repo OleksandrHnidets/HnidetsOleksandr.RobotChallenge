7f0a0f96ebb3e3d5e9a2361d27f9f508094375c7
Gnidec Oleksander <sgnidet@gmail.com>
2021-09-13 00:48:59 +0300

    UnitTestCreation

---
+++ HnidetsOleksandr.RobotChallenge.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HnidetsOleksandr.RobotChallenge.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HnidetsOleksandr.RobotChallenge.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9879af6b-0cab-485b-831b-47d2a440aa25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
