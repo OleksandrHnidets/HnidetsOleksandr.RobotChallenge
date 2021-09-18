a9bf611ebfa6f0e1f34152385330d53246d2fd30
Gnidec Oleksander <sgnidet@gmail.com>
2021-09-18 21:37:35 +0300

    21:37 attempt

--- HnidetsOleksandr.RobotChallenge/DistanceHelper.cs
+++ HnidetsOleksandr.RobotChallenge/DistanceHelper.cs
@@ -9,7 +9,70 @@ namespace HnidetsOleksandr.RobotChallenge
 {
     class DistanceHelper
     {
+        public const int MinLinearDistance = 9;
+        public const int MinDiagonalDistance = 18;
+        
         public static int FindDistance(Position a, Position b)
             => (int)(Math.Pow(a.X - b.X, 2) + Math.Pow(a.Y - b.Y, 2));
+
+        public static bool IsInRadius(Robot.Common.Robot movingRobot, Position stationPos)
+        {
+            List<Position> radiusCells = new List<Position>()
+            {
+                new Position(){ X=stationPos.X+1, Y=stationPos.Y+1},
+                new Position(){X=stationPos.X+2, Y=stationPos.Y+2},
+                new Position(){X=stationPos.X+3, Y=stationPos.Y+3},
+                new Position(){X=stationPos.X+1, Y=stationPos.Y+2},
+                new Position(){X=stationPos.X+1, Y=stationPos.Y+3},
+                new Position(){X=stationPos.X+2, Y=stationPos.Y+1},
+                new Position(){X=stationPos.X+2, Y=stationPos.Y+3},
+                new Position(){X=stationPos.X+3, Y=stationPos.Y+1},
+                new Position(){X=stationPos.X+3, Y=stationPos.Y+2},
+
+                new Position(){X=stationPos.X+1, Y=stationPos.Y-1},
+                new Position(){X=stationPos.X+2, Y=stationPos.Y-2},
+                new Position(){X=stationPos.X+3, Y=stationPos.Y-3},
+                new Position(){X=stationPos.X+1, Y=stationPos.Y-2},
+                new Position(){X=stationPos.X+1, Y=stationPos.Y-3},
+                new Position(){X=stationPos.X+2, Y=stationPos.Y-1},
+                new Position(){X=stationPos.X+2, Y=stationPos.Y-3},
+                new Position(){X=stationPos.X+3, Y=stationPos.Y-1},
+                new Position(){X=stationPos.X+3, Y=stationPos.Y-2},
+
+                new Position(){ X=stationPos.X-1, Y=stationPos.Y+1},
+                new Position(){X=stationPos.X-2, Y=stationPos.Y+2},
+                new Position(){X=stationPos.X-3, Y=stationPos.Y+3},
+                new Position(){X=stationPos.X-1, Y=stationPos.Y+2},
+                new Position(){X=stationPos.X-1, Y=stationPos.Y+3},
+                new Position(){X=stationPos.X-2, Y=stationPos.Y+1},
+                new Position(){X=stationPos.X-2, Y=stationPos.Y+3},
+                new Position(){X=stationPos.X-3, Y=stationPos.Y+1},
+                new Position(){X=stationPos.X-3, Y=stationPos.Y+2},
+
+                new Position(){ X=stationPos.X-1, Y=stationPos.Y-1},
+                new Position(){X=stationPos.X-2, Y=stationPos.Y-2},
+                new Position(){X=stationPos.X-3, Y=stationPos.Y-3},
+                new Position(){X=stationPos.X-1, Y=stationPos.Y-2},
+                new Position(){X=stationPos.X-1, Y=stationPos.Y-3},
+                new Position(){X=stationPos.X-2, Y=stationPos.Y-1},
+                new Position(){X=stationPos.X-2, Y=stationPos.Y-3},
+                new Position(){X=stationPos.X-3, Y=stationPos.Y-1},
+                new Position(){X=stationPos.X-3, Y=stationPos.Y-2},
+            };
+            if (movingRobot.Position.X == stationPos.X || movingRobot.Position.Y == stationPos.Y)
+            {
+                bool isTrue = (FindDistance(movingRobot.Position, stationPos) <= 9);
+                return isTrue;
+            }
+            else
+            {
+                foreach(Position p in radiusCells)
+                {
+                    return (p == movingRobot.Position);
+                }
+            }
+
+            return false;   
+        }
     }
 }
--- HnidetsOleksandr.RobotChallenge/HnidetsOleksandr.cs
+++ HnidetsOleksandr.RobotChallenge/HnidetsOleksandr.cs
@@ -9,6 +9,8 @@ namespace HnidetsOleksandr.RobotChallenge
 {
     public class HnidetsOleksandr : IRobotAlgorithm
     {
+        public const int MinEnergyToCreateRobot = 250;
+
         public string Author
         {
             get { return "Hnidets Oleksandr"; }
@@ -17,20 +19,25 @@ namespace HnidetsOleksandr.RobotChallenge
         public RobotCommand DoStep(IList<Robot.Common.Robot> robots, int robotToMoveIndex, Map map)
         {
             Robot.Common.Robot movingRobot = robots[robotToMoveIndex];
-            if((movingRobot.Energy>500)&&(robots.Count<map.Stations.Count))
-            {
-                return new CreateNewRobotCommand();
-            }
-
             Position stationPosition = FindNearestStation(robots[robotToMoveIndex], map, robots);
 
-            if (stationPosition == null)
-                return null;
-            if (stationPosition == movingRobot.Position)
-                return new CollectEnergyCommand();
-            else 
+            if (DistanceHelper.IsInRadius(movingRobot, stationPosition))
             {
-                return new MoveCommand() { NewPosition = stationPosition };
+                return new CollectEnergyCommand();
+            }
+            else
+            if ((stationPosition.X == movingRobot.Position.X) || (stationPosition.Y== movingRobot.Position.Y))
+            {
+                return new MoveCommand() { NewPosition = MoveLinear(movingRobot.Position, stationPosition) };
+            }
+            else
+            if((stationPosition.X!= movingRobot.Position.X) && (stationPosition.Y!=movingRobot.Position.Y))
+            {
+                return new MoveCommand() { NewPosition = MoveDiagonal(movingRobot, stationPosition) };
+            }
+            else
+            {
+                return new CollectEnergyCommand();
             }
         }
 
@@ -69,6 +76,42 @@ namespace HnidetsOleksandr.RobotChallenge
 
         public bool IsStationFree(EnergyStation station, Robot.Common.Robot movingRobot, IList<Robot.Common.Robot> robots)
         => IsCellFree(station.Position, movingRobot, robots);
+
+        public Position MoveDiagonal(Robot.Common.Robot movingRobot, Position stationPos)
+        {
+            if (movingRobot.Position.X < stationPos.X && movingRobot.Position.Y < stationPos.Y)
+            {
+                return new Position() { X = movingRobot.Position.X + 1, Y = movingRobot.Position.Y + 1 };
+            }
+            else if (movingRobot.Position.X < stationPos.X && movingRobot.Position.Y > stationPos.Y)
+            {
+                return new Position() { X = movingRobot.Position.X + 1, Y = movingRobot.Position.Y - 1 };
+            }
+            else if (movingRobot.Position.X > stationPos.X && movingRobot.Position.Y < stationPos.Y)
+            {
+                return new Position() { X = movingRobot.Position.X - 1, Y = movingRobot.Position.Y + 1 };
+            }
+            else if (movingRobot.Position.X > stationPos.X && movingRobot.Position.Y > stationPos.Y)
+            {
+                return new Position() { X = movingRobot.Position.X - 1, Y = movingRobot.Position.Y - 1 };
+            }
+            else return new Position() { X = movingRobot.Position.X, Y = movingRobot.Position.Y };
+        }
+
+        public Position MoveLinear(Position movingRobotPos, Position stationPos)
+        {
+            if (movingRobotPos.X == stationPos.X)
+            {
+                Position pos = (movingRobotPos.Y < stationPos.Y) ? (new Position() { X = movingRobotPos.X, Y = movingRobotPos.Y + 1 }) : (new Position() { X = movingRobotPos.X, Y = movingRobotPos.Y - 1 });
+                return pos;
+            }
+            else if (movingRobotPos.Y == stationPos.Y)
+            {
+                Position pos = (movingRobotPos.X < stationPos.X) ? (new Position() { X = movingRobotPos.X + 1, Y = movingRobotPos.Y }) : (new Position() { X = movingRobotPos.X - 1, Y = movingRobotPos.Y });
+                return pos;
+            }
+            else return movingRobotPos;
+        }
     }
 
 }
